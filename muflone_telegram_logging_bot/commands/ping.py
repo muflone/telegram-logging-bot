c7a34793274d9f27c2ce017d56755af2b4a953f7
Muflone <webreg@muflone.com>
2026-08-10 23:02:42 +0200

    Added command ping

---
+++ muflone_telegram_logging_bot/commands/ping.py
@@ -0,0 +1,39 @@
+##
+#     Project: Telegram Logging Bot
+# Description: Telegram bot to log messages in Telegram groups
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2026 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+from typing import Optional
+
+import telegram
+import telegram.ext
+
+from .base import BaseCommand
+
+
+class CommandPing(BaseCommand):
+    command_name: Optional[str] = 'ping'
+    description: Optional[str] = 'Ping if the bot is alive'
+
+    def get_reply_text(self,
+                       update: telegram.Update,
+                       context: telegram.ext.ContextTypes.DEFAULT_TYPE,
+                       *args,
+                       **kwargs
+                       ) -> Optional[str]:
+        return 'PONG!'

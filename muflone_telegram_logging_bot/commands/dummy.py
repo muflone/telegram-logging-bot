b9a6a19bb4173da49a5d174eab2468ba330f841c
Muflone <webreg@muflone.com>
2026-08-12 01:53:39 +0200

    Added command dummy

---
+++ muflone_telegram_logging_bot/commands/dummy.py
@@ -0,0 +1,26 @@
+##
+#     Project: Telegram Logging Bot
+# Description: Telegram bot to log messages in Telegram groups
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2026 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+from .base import BaseCommand
+
+
+class CommandDummy(BaseCommand):
+    trigger = 'dummy'
+    description = 'Dummy command'

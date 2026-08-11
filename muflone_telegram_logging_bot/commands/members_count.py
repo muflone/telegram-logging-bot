f6966bc1c1797a22e1ebfc5a254aeed86c6d290c
Muflone <webreg@muflone.com>
2026-08-11 03:28:49 +0200

    Added command members_count

---
+++ muflone_telegram_logging_bot/commands/members_count.py
@@ -0,0 +1,121 @@
+##
+#     Project: Telegram Logging Bot
+# Description: Telegram bot to log messages in Telegram groups
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2026 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+from typing import TYPE_CHECKING
+
+import telegram
+
+from .base import BaseCommand
+from ..database import Database
+from .. import extras
+
+if TYPE_CHECKING:
+    import sqlite3
+    from typing import Optional
+
+    import telegram.ext
+
+
+class CommandMembersCount(BaseCommand):
+    command_name: Optional[str] = 'members_count'
+    description: Optional[str] = 'Collect members count statistics'
+
+    def get_reply_text(self,
+                       update: telegram.Update,
+                       context: telegram.ext.ContextTypes.DEFAULT_TYPE,
+                       *args,
+                       **kwargs
+                       ) -> Optional[str]:
+        return 'Members count statistics collected'
+
+    async def execute(self,
+                      update: telegram.Update,
+                      context: telegram.ext.ContextTypes.DEFAULT_TYPE,
+                      *args,
+                      **kwargs
+                      ) -> None:
+        """
+        Get reply text and send response
+        """
+        await self.collect_all_member_count(source=f'/{self.command_name}')
+        return await super().execute(update=update,
+                                     context=context,
+                                     args=args,
+                                     kwargs=kwargs)
+
+    async def collect_all_member_count(self,
+                                       source: str,
+                                       ) -> dict[int, int]:
+        result = {}
+        for chat_id, db_path in self.bot.databases.get_known_groups().items():
+            database = Database(filepath=db_path)
+            try:
+                member_count = await self.bot.bot.get_chat_member_count(
+                    chat_id=chat_id)
+                with database.open() as connection:
+                    self.update_database_schema(connection=connection)
+                    connection.execute(
+                        '''
+                        INSERT INTO members_count__member_count (
+                            chat_id,
+                            member_count,
+                            source,
+                            taken_at
+                        )
+                        VALUES (?, ?, ?, ?)
+                        ''',
+                        (
+                            chat_id,
+                            member_count,
+                            source,
+                            extras.utc_now_iso(),
+                        ),
+                    )
+                    connection.commit()
+            except telegram.error.TelegramError as error:
+                print({
+                    'event': 'members_count_all_member_count_error',
+                    'chat_id': chat_id,
+                    'error': str(error),
+                })
+                continue
+            result[chat_id] = member_count
+            print({
+                'chat_id': chat_id,
+                'member_count': member_count,
+                'source': source,
+            })
+        return result
+
+    def update_database_schema(self,
+                               connection: sqlite3.Connection,
+                               ) -> None:
+        connection.executescript(
+            '''
+            CREATE TABLE IF NOT EXISTS members_count__member_count (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                chat_id INTEGER NOT NULL,
+                member_count INTEGER NOT NULL,
+                source TEXT NOT NULL,
+                taken_at TEXT NOT NULL
+            );
+            '''
+        )
+        return super().update_database_schema(connection=connection)
